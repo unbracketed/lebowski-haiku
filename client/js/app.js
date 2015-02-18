a131fdf8a6f7983969ca5bfe4e5a7cdc67335a48
Brian Luft <brian@lincolnloop.com>
2015-02-18 08:35:43 -0800

    WIP start adding in support for async fetching on route change

--- client/js/app.js
+++ client/js/app.js
@@ -1,7 +1,56 @@
-'use strict'
 var React = require('react')
 
+
+var getText = function(url) {
+  return new Promise((resolve, reject) => {
+    var req = new XMLHttpRequest();
+    req.onload = function () {
+      if (req.status === 404) {
+        reject(new Error('not found'));
+      } else {
+
+
+        resolve(req.response)
+
+
+      }
+    };
+    req.open('GET', url);
+    req.send();
+  });
+}
+
 var App = React.createClass({
+
+  statics: {
+    fetchData (params) {
+      // return getJSON(`${API}/contacts`).then((res) => res.contacts);
+      return getText('https://raw.githubusercontent.com/unbracketed/lebowski/master/phrases_5.txt')
+    }
+  },
+
+  getInitialState () {
+    return { loading: false };
+  },
+
+  componentDidMount () {
+    var timer;
+    loadingEvents.on('loadStart', () => {
+      clearTimeout(timer);
+      // for slow responses, indicate the app is thinking
+      // otherwise its fast enough to just wait for the
+      // data to load
+      timer = setTimeout(() => {
+        this.setState({ loading: true });
+      }, 300);
+    });
+
+    loadingEvents.on('loadEnd', () => {
+      clearTimeout(timer);
+      this.setState({ loading: false });
+    });
+  },
+
   render: function () {
     return (
       <div>Abide</div>
--- client/js/index.js
+++ client/js/index.js
@@ -5,13 +5,29 @@ var log = require('loglevel')
 var React = require('react')
 var Router = require('react-router')
 var routes = require('./routes')
+var EventEmitter = require('events').EventEmitter;
+var loadingEvents = new EventEmitter();
 
 // Log level setup
 // if (config.debug) {
 //   log.setLevel('debug');
 // }
 
+function fetchData(routes, params) {
+  var data = {};
+  return Promise.all(routes
+    .filter(route => route.handler.fetchData)
+    .map(route => {
+      return route.handler.fetchData(params).then(d => {data[route.name] = d;});
+    })
+  ).then(() => data);
+}
 
-Router.run(routes, function (Handler) {
-  React.render(<Handler/>, document.body)
+Router.run(routes, function (Handler, state) {
+  loadingEvents.emit('loadStart');
+
+  fetchData(state.routes, state.params).then((data) => {
+    loadingEvents.emit('loadEnd');
+    React.render(<Handler/>, document.body)
+  })
 })
