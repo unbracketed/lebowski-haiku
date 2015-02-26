9cb1f382640564235cb38f4d618ef8b20924c78f
Brian Luft <brian@lincolnloop.com>
2015-02-26 09:04:25 -0800

    Build an absolute path for static files

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 var express = require('express')
 var app = express()
+var path = require('path')
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
   res.sendFile('index.html', {root: 'templates'})
 })
-app.use(express.static('build'))
+app.use(express.static(path.join(__dirname, '..', 'build')))
 
 var server = app.listen((process.env.PORT || 10000), function () {
 
